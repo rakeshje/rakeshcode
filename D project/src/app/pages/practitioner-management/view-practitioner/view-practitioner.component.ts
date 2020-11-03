59758256c4d382f9e94ba6cbe211fe5d11b4881d
Rakesh Kumar <ph-rakeshkumar@mobiloitte.com>
2020-11-03 19:36:08 +0530

    UI design+api integration of practioner+view practioner+validation+navigation

--- D project/src/app/pages/practitioner-management/view-practitioner/view-practitioner.component.ts
+++ D project/src/app/pages/practitioner-management/view-practitioner/view-practitioner.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MainService } from 'src/app/provider/main.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-practitioner',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-practitioner.component.css']
 })
 export class ViewPractitionerComponent implements OnInit {
+  userId: any;
+  viewData: any;
 
-  constructor() { }
+  constructor(public mainService: MainService, public active:ActivatedRoute)
+   {
+     this.active.queryParams.subscribe((params)=>{
+       this.userId=params.value
+       console.log('hfhf', this.userId);
+       
+     })
+    }
 
   ngOnInit() {
+    this.viewPractioner();
+  }
+  // view practioner
+  viewPractioner(){
+    
+    this.mainService.showSpinner();
+    this.mainService.getApi("admin/viewPractitioner?userId=this.userId",1).subscribe((res)=>{
+      if(res.responseCode==200){
+        this.mainService.hideSpinner();
+        this.viewData=res.result
+      }
+    },(error)=>{
+      this.mainService.hideSpinner();
+      this.mainService.errorToast('something went wrong')
+    })
+
   }
 
 }
