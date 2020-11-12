0117c12c94331285964dd051697864b9ac494a53
Rakesh Kumar <ph-rakeshkumar@mobiloitte.com>
2020-11-12 12:46:35 +0530

    Ui design+manage api+sonar bugs fix

--- D project/src/app/pages/dashboard/user-management/add-user/add-user.component.ts
+++ D project/src/app/pages/dashboard/user-management/add-user/add-user.component.ts
@@ -7,40 +7,14 @@ import { MainService } from 'src/app/provider/main.service';
   styleUrls: ['./add-user.component.css']
 })
 export class AddUserComponent implements OnInit {
-  customerData: any;
-  customerLength: any;
-  status: any;
-  currentPage: any;
-  itemPerPage: any;
+  
 
   constructor(public mainService:MainService) { }
 
   ngOnInit() {
   }
 
-  // get customer
-  getCustomer(){
-    this.mainService.showSpinner();
-    let data ={
-      'page':this.currentPage,
-      'limit':this.itemPerPage,
-    }
-    this.mainService.postApi('admin/listUsers',data, 1).subscribe((res:any)=>{
-      
-      if(res.responseCode==200){
-        this.mainService.hideSpinner();
-        this.customerData=res.result.docs;
-        this.customerLength=res.result.total;
-        this.status=res.result.docs[0].status;
-        console.log("f", this.customerLength);
-        
-      }
-      
-    },(error)=>{
-      this.mainService.hideSpinner();
-      this.mainService.errorToast('something went wrong')
-    })
-  }
+  
 
 
 }
