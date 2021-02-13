bebe0c6f62f68aa7e5cc44f9ff416de61f34dc3a
rakeshje <62991059+rakeshje@users.noreply.github.com>
2021-02-13 17:04:15 +0530

    Delete D project directory

--- D project/sonarqube.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-/home/administrator/sonar-scanner-4.3.0.2102/bin/sonar-scanner \
-  -Dsonar.projectKey=developer-needed-forcreating-20053694-angular-ph-rakeshkumar/
-  -Dsonar.sources=. \
-  -Dsonar.host.url=http://172.16.0.220:9000 \
-  -Dsonar.login=32cf9325138f97409198a1a1373cd27f6c9cdded \
-  -Dsonar.exclusions=node_modules/**,dist/**
-
-
